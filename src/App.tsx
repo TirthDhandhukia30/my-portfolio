8721acd508d616ae7a7b61d36ce184fac227d675
Tirth Dhandhukia <tirth30.info@gmail.com>
2025-12-27 14:42:14 +0530

    feat: core application structure with global styles and theme support

---
+++ src/App.tsx
@@ -0,0 +1,41 @@
+import { BrowserRouter as Router } from 'react-router-dom';
+import { Analytics } from '@vercel/analytics/react';
+import { ThemeProvider } from './contexts/ThemeContext';
+import { TooltipProvider } from './components/Tooltip';
+import { IntroOverlay } from './components/IntroOverlay';
+import { Navbar } from './components/Navbar';
+import { Footer } from './components/Footer';
+import { CliSection } from './sections/CliSection';
+import { HeroSection } from './sections/HeroSection';
+import { ActivitySection } from './sections/ActivitySection';
+import { ProjectsSection } from './sections/ProjectsSection';
+import { StackSection } from './sections/StackSection';
+import { GitHubContributionsSection } from './sections/GitHubContributionsSection';
+import { ContactSection } from './sections/ContactSection';
+
+function App() {
+  return (
+    <Router>
+      <ThemeProvider>
+        <div id="name" className="max-w-[920px] mx-auto relative scroll-pt-[90px]">
+          <IntroOverlay />
+          <TooltipProvider />
+          <Navbar />
+          <main>
+            <CliSection />
+            <HeroSection />
+            <ActivitySection />
+            <GitHubContributionsSection />
+            <ProjectsSection />
+            <StackSection />
+            <ContactSection />
+          </main>
+          <Footer />
+        </div>
+        <Analytics />
+      </ThemeProvider>
+    </Router>
+  );
+}
+
+export default App;
